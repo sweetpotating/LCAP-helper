b7180c4a966e35ea3b1e1f9aa3c871982b34a490
sweetpotating <tanyueting96@gmail.com>
2026-04-28 19:08:57 +0800

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import SmartCollabHelper from "./SmartCollabHelper.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <SmartCollabHelper />
+  </React.StrictMode>
+);
